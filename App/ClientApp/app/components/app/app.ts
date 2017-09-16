f0c5bb2e652fed71ec6517480edd8280b10dcad9
Jonathan Mezach <jonathanm@infosupport.com>
2017-09-16 10:28:00 +0200

    Initial code

---
+++ App/ClientApp/app/components/app/app.ts
@@ -0,0 +1,20 @@
+import { Aurelia, PLATFORM } from 'aurelia-framework';
+import { Router, RouterConfiguration } from 'aurelia-router';
+
+export class App {
+    router: Router;
+
+    configureRouter(config: RouterConfiguration, router: Router) {
+        config.title = 'Aurelia';
+        config.map([{
+            route: [ '', 'home' ],
+            name: 'home',
+            settings: { icon: 'home' },
+            moduleId: PLATFORM.moduleName('../home/home'),
+            nav: true,
+            title: 'Home'
+        }]);
+
+        this.router = router;
+    }
+}
