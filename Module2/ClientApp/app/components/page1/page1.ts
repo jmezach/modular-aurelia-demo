59b226da85d6a65291c980463ba9a0db1a8347e9
Jonathan Mezach <jonathanm@infosupport.com>
2017-10-10 15:31:21 +0200

    Add fetching of data from the backend

--- Module2/ClientApp/app/components/page1/page1.ts
+++ Module2/ClientApp/app/components/page1/page1.ts
@@ -1,3 +1,21 @@
+import { HttpClient } from 'aurelia-fetch-client';
+import { inject } from 'aurelia-framework';
+
+@inject(HttpClient)
 export class Page1 {
-    
+    public forecasts: WeatherForecast[];
+    constructor(http: HttpClient) {
+        http.fetch('api/SampleData/WeatherForecasts')
+            .then(result => result.json() as Promise<WeatherForecast[]>)
+            .then(data => {
+                this.forecasts = data;
+            });
+    }
+}
+
+interface WeatherForecast {
+    dateFormatted: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
 }
